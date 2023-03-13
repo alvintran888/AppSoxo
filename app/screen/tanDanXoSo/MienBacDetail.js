0ecec029a0d2c20dfe1564c497886299dbf33524
dongnv <dongnv@tandan.com.vn>
2023-03-13 17:39:27 +0700

    mienbaclai

--- app/screen/tanDanXoSo/MienBacDetail.js
+++ app/screen/tanDanXoSo/MienBacDetail.js
@@ -7,11 +7,10 @@ import {
   ScroolView,
   TouchableOpacity,
   FlatList,
+  Image
 } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome5Pro';
 import { Header } from '@app/components';
-import { DatePickerIOSBase } from 'react-native';
-import { getApiLevel } from 'react-native-device-info';
 import moment from 'moment';
 
 
@@ -19,13 +18,51 @@ const MienBacDetail = () => {
     const navigation = useNavigation();
     const [open, setOpen] = useState(false);
     const [date, setDate] = useState(new Date());
+    const [dataNV, setDataNV] = useState([]);
+    const [maDb, setMaDb] = useState([]);
+
+    const _renderItem = ({item}) => {
+        return (
+           <TouchableOpacity style={{flexDirection:'row', margin: 10, padding:10}}>
+                {/* image */}
+                <View style={{flex:1, }}>
+
+                <Image source={{uri: item.avatar}} style={{height:100, width:120, borderRadius:20}}/>
+
+                </View>
+                    {/* ten */}
+                <View style={{flex: 2 }}>
+                    <View style={{ flex:0.3, flexDirection: 'row', marginLeft: 15, marginTop: 5,}}>
+                        <Text style={{ fontSize: 20, paddingRight: 5 }}>{item.first_name}</Text>
+                        <Text style={{ fontSize: 20 }}>{item.last_name}</Text>
+                    </View>
+
+                    <View style={{flex:3, marginLeft: 15,}}>
+                    <Text style={{ fontSize: 13 }}>Email:{item.email}</Text>
+                    </View>
+                </View>
+                {/* email */}
+                
+            
+          </TouchableOpacity>
+      
+        );
+      };
+
     useEffect(() => {
-        getAPI();
-        return () => {};
-    }, [date]);
+
+        const fetchData = async () => {
+          const res = await fetch('https://api.xoso.me/app/json-kq-mienbac?name=KQXS&v=2&ngay_quay=2023-03-12');
+          const json = await res.json();
+          console.log(json.data.lotData);
+        }
+        
+        fetchData();
+      },[])
 
 
     return (
+        
         <View style={{flex:1}}>
 
             <Header
@@ -35,55 +72,40 @@ const MienBacDetail = () => {
                     isStack={'true'}
                     
             />
-
-            <View style={{ flex: 1 }}>
-                <View style={{ height: 80, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
-                    <View style={{}}>
-                        <Text style={{ fontSize: 18, fontWeight: '600', color: 'red' }}>
-                            Xổ số Miền Bắc (Hà Nội)
-                        </Text>
-                    </View>
-
-                    {/* <View style={{marginTop:5}}>
-                        <Text style={{fontWeight:'300', color:'black'}}>
-                        Thứ 5, 16/02/2023
-                        </Text>
-                    </View> */}
-                    <View>
-                        <TouchableOpacity style={{ backgroundColor: 'white', borderRadius: 50 }} onPress={() => setOpen(true)}>
-                            <Text style={{}}>
-                                {moment(date).format('YYYY-MM-DD')}
+           
+                <View style={{ flex: 1 }}>
+                   
+                    <View style={{ height: 80, backgroundColor: '#FFFFFF', justifyContent: 'center', alignItems: 'center' }}>
+                        <View style={{}}>
+                            <Text style={{ fontSize: 18, fontWeight: '600', color: 'red' }}>
+                                Xổ số Miền Bắc (Hà Nội)
                             </Text>
-                        </TouchableOpacity>
+                        </View>
 
-                        <DatePickerIOSBase
-                            style={{}}
-                            modal
-                            open={open}
-                            date={date}
-                            onConfirm={date => {
-                                setOpen(false);
-                                setDate(date);
-                            }}
-                            onCancel={() => {
-                                setOpen(false);
-                            }}
-                        />
+                        <View>
+                            <TouchableOpacity style={{ backgroundColor: 'white', borderRadius: 50 }} onPress={() => setOpen(true)}>
+                                <Text style={{}}>
+                                    {moment(date).format('YYYY-MM-DD')}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+
                     </View>
+                    <View style={{ height: 1, backgroundColor: '#CFCFCF' }} />
+
+                
+                    <View style={{height:40, width:'100%', flexDirection:'row', backgroundColor:'red', }}>
+                        <Text></Text>
+
+                    </View>
+                
+
                 </View>
-                <View style={{ height: 1, backgroundColor: '#CFCFCF' }} />
 
-
-                <View style={{}}>
-
-                </View>
-
-
-            </View>
-
-
+            
 
          </View>
+         
             
     )
 }
