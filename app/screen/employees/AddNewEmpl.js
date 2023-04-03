884cc90696648a7aa43d025d4bcd30ee1c8a8c2e
nghia <you@example.com>
2023-04-03 23:48:27 +0700

    chuaduocCRUD

--- app/screen/employees/AddNewEmpl.js
+++ app/screen/employees/AddNewEmpl.js
@@ -7,43 +7,77 @@ const AddNewEmpl = () => {
     const [firstName, setFirstname] = useState('Nghia');
     const [lastName, setLastname] = useState('Tran');
     const [email, setEmail] = useState('Chloe@gmail.com');
+    
 
-    const SaveEmpl = () => {
+    const SaveEmpl = async () => {
+        const response = await fetch ('https://reqres.in/api/users?page=2', {
+            method: "POST",
+            header: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                email,
+            }),
+        })
+        if (response.ok) {
+            console.log("User added successfully");
+            console.log(response.SaveEmpl) 
+            
+          } else {
+            console.log("Error adding user");
+          }
         //Tạo dữ liệu
-        let dulieu = {first: firstName, last:lastName, email: email}
+        // let dulieu = {first: firstName, last:lastName, email: email}
 
-        fetch('https://reqres.in/api/users?page=2', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify( dulieu )
-        })
+        // fetch('https://reqres.in/api/users?page=2', {
+        //   method: 'POST',
+        //   headers: {
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify( dulieu )
+        // })
 
-        .then ((res) => {
-                if(res.status ==201)
-                alert ("Add Successfull")
-        })
-        .catch ((ex) => {
-            console.log(ex);
-        })
+        // .then ((res) => {
+        //         if(res.status ==201)
+        //         alert ("Add Successfull")
+        // })
+        // .catch ((ex) => {
+        //     console.log(ex);
+        // })
+        
     }
     
     return(
         <View style={{flex:1}}>
-            <Header title={'New Employee'} backgroundColor={'#849FE3'} isStack={'True'}/> 
+            <Header title={'New Employee'} backgroundColor={'#849FE3'} isStack={'True'}/>  
 
-            
-            <TextInput placeholder='First Name'
+            <TextInput 
+            placeholder='First Name'
+            value={firstName}
+            onChangeText={setFirstname}
+            />
+
+            <TextInput 
+            placeholder='Last Name'
+            value={lastName}
+            onChangeText={setLastname}
+            />
+
+            <TextInput 
+            placeholder='Email'
+            value={email}
+            onChangeText={setEmail}
+            />
+
+
+            {/* <TextInput 
+            placeholder='First Name'
             onChangeText={(txt) => {setFirstname(txt)}}
-            />
-            <TextInput placeholder='Last Name'
-            onChangeText={(txt) => {setLastname(txt)}}
-            />
-            <TextInput placeholder='Email'
-            onChangeText={(txt) => {setEmail(txt)}}
-            />
+            /> */}
+            
             <Button title="Save" onPress={SaveEmpl}/>
             
             
